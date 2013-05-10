96bd0e69eca0d373c2a9e160aaf848e902667510
Andrew Mazzarella <github@myimedia.com>
2013-05-10 11:22:22 -0300

    added limit var and round on per sec

--- iteration-test.r
+++ iteration-test.r
@@ -1,10 +1,11 @@
 REBOL [Title: Incrimentor Test]
 start: now/time/precise
 duration: start - now/time/precise
+limit: -00:00:10
 i: 0
-while [duration > -00:00:05] [
+while [duration > limit] [
   duration: start - now/time/precise
 	i: i + 1
 ]
-print [i "iterations in 5 seconds"]
-print [i / 60 "per second"]
+print [i "iterations in" limit "seconds"]
+print [round i / 60 "per second"]
